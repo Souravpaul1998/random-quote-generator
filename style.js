0ff8930de93ae37253a2888b29e5ee4ed6d086b6
Sourav Paul <paulsouravsoumen@gmail.com>
2025-03-20 02:30:07 +0530

    Added comment for the JS Code

--- style.js
+++ style.js
@@ -1,22 +1,23 @@
-const quote = document.getElementById("quote")
+const quote = document.getElementById("quote") 
 const author = document.getElementById("author")
 
 const api_url = "https://api.freeapi.app/api/v1/public/quotes/quote/random";
 
-async function getquote(url) {
-    const response = await fetch(url);
-    var data = await response.json();
+async function getquote(url) {     // async function handles API calls withouy blocking other tasks.
+    const response = await fetch(url);     // fetch(url) sends request to the API.
+    var data = await response.json();    // await response.json() converts the response to the JSON format
     console.log(data)
     quote.innerHTML = `"${data.data.content}"`;
     author.innerHTML = `----by ${data.data.author}`;
     
 }
 getquote(api_url);
-
+ //method to tweet
 function tweet(){
+    // open twitter
     window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + " " +  author.innerHTML )
 }
-
+// method for copy
 function copyText(){
     let text = document.getElementById("quote");
         console.log(text.innerText)
